464f89ba6a04c3fa3475a233af0c585c0e9c2dd1
_Wizz_ <142069423+Hot-Wizard@users.noreply.github.com>
2025-09-01 11:23:33 -0300

    Interaction and Movement Done

--- BlueDreamsUnity/Assets/Script/Input/PlayerController.cs
+++ BlueDreamsUnity/Assets/Script/Input/PlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 
-public class PlayerMovementController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private Transform groundCheck;
@@ -12,8 +12,6 @@ public class PlayerMovementController : MonoBehaviour
     private float gravity = -9.81f;
     private float groundDistance = 0.4f;
     private bool IsGrounded;
-    public bool IsOnDryLand = true;
-    public float alteratedSpeed = 1f;
 
     void FixedUpdate()
     {
@@ -22,7 +20,7 @@ public class PlayerMovementController : MonoBehaviour
         if (IsGrounded && velocity.y < 0) velocity.y = -2f;
 
         Vector3 move = transform.right * InputManager._instance.xzPlayer.x + transform.forward * InputManager._instance.xzPlayer.y;
-        controller.Move(move * speed * alteratedSpeed *Time.fixedDeltaTime);
+        controller.Move(move * speed  *Time.fixedDeltaTime);
 
         velocity.y += gravity * Time.fixedDeltaTime;
 
---
+++ BlueDreamsUnity/Assets/Script/Interactables/PlayerInteractables.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractables : MonoBehaviour
+{
+    [SerializeField] private Camera myCam; 
+    private float rayDis = 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteractables();
+    }
+    void CheckInteractables()
+    {
+        RaycastHit hit;
+        Vector3 rayOrigin = myCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.5f));
+
+        if(Physics.Raycast(rayOrigin, myCam.transform.forward, out hit, rayDis))
+        {
+            Interactable interactable = hit.collider.GetComponent<Interactable>();
+            if(interactable != null)
+            {
+                InteractionUIManager._instance.TriggerCursor(true);
+            }
+        }
+    }
+}
