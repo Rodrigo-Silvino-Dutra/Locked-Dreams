87c7c5110caaa0a08ffe9aa1191aeccb7e748d40
HotWizard <142069423+Hot-Wizard@users.noreply.github.com>
2025-10-02 22:27:53 -0300

    betterdream2

---
+++ BlueDreamsUnity/Assets/Script/Interactables/Dream2/PC.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PC : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Transform holdingObject;
+    [SerializeField] private GameObject screen;
+    private string pc = "Linux";
+
+    public void OnFocusEnter()
+    {
+    }
+    public void OnFocusExit()
+    {
+    }
+    public void OnInteract()
+    {
+        Transform cd = holdingObject.GetChild(0);
+        if (ProgressionDream2._instance.isHoldingCD && cd != null)
+        {
+            Debug.Log("PEGOU O CD E ESTA TENTANDO INTERAGIR");
+            if (cd.CompareTag(pc))
+            {
+                Destroy(cd.gameObject);
+                screen.SetActive(true);
+                ProgressionDream2._instance.linuxBooted = true;
+
+            }
+        }
+    }
+}
--- BlueDreamsUnity/Assets/Script/Interactables/Dream2/ProgressionDream2.cs
+++ BlueDreamsUnity/Assets/Script/Interactables/Dream2/ProgressionDream2.cs
@@ -6,7 +6,6 @@ public class ProgressionDream2 : MonoBehaviour
     public bool isholdingStarTrekCharacter, isHoldingCD, jupiterInPlace, linuxBooted;
     public GameObject galileoPuzzle;
     public bool[] dollsPlaced = new bool[3];//data = 0; picard = 1; worf = 2;
-
     void Start()
     {
         isholdingStarTrekCharacter = false;
