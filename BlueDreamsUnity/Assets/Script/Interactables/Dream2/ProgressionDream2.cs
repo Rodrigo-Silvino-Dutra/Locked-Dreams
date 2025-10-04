da8455fc2242e4dc7f17c12ae82f092e9125ff4d
HotWizard <142069423+Hot-Wizard@users.noreply.github.com>
2025-10-04 00:36:49 -0300

    Dream2 puzzle completly done

--- BlueDreamsUnity/Assets/Script/Interactables/Dream2/ProgressionDream2.cs
+++ BlueDreamsUnity/Assets/Script/Interactables/Dream2/ProgressionDream2.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class ProgressionDream2 : MonoBehaviour
 {
+    public Animator anim;
+
     public static ProgressionDream2 _instance;
     public bool isholdingStarTrekCharacter, isHoldingCD, jupiterInPlace;
     public GameObject galileoPuzzle;
+
     public bool[] dollsPlaced = new bool[3];//data = 0; picard = 1; worf = 2;
     void Start()
     {
@@ -25,7 +28,7 @@ public class ProgressionDream2 : MonoBehaviour
     {
         if (dollsPlaced[0] && dollsPlaced[1] && dollsPlaced[2])
         {
-            
+            anim.enabled = true;
         }
     }
 }
